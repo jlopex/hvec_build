af814078ae53cb40ca28faa976680c0def175b63
root <root@localhost>
2014-08-20 12:51:10 +0200

    install_cmake: fix build

--- install_cmake.sh
+++ install_cmake.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [ "$(uname -m)" = "x86_64" ]; then
-  ARCHOPTS="--enable-pic"
- else
-  ARCHOPTS=""
-fi
+FILE=cmake-3.0.0.tar.gz
 
 echo "Installing CMAKE"
-wget http://www.cmake.org/files/v3.0/cmake-3.0.0.tar.gz &&
-tar xvzf cmake-3.0.0.tar.gz && \
-cd $HOME/vlc_build/cmake-3.0.0 && \
-./configure --prefix=$HOME/vlc_build/vlcdeps/usr \
-            $ARCHOPTS && \
+
+if [ ! -f $FILE ]; then
+    echo "File not found!"
+    wget http://www.cmake.org/files/v3.0/$FILE
+fi
+
+tar xvzf $FILE && \
+cd $HOME/hvec_build/cmake-3.0.0 && \
+./configure --prefix=$HOME/hvec_build/hvecdeps/usr && \
 make && make install
 
